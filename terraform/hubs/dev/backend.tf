35d83dbebee7ac7e8ed71e5a82f33b56bf005a0e
Babasanmi Adeyemi <boadeyem@iu.edu>
2025-09-10 02:50:11 -0400

    pre-terraform validate

--- terraform/hubs/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "--envs-4852"
-    key            = "--hub/${var.environment}/terraform.tfstate"
-    region         = ""
-    use_lockfile   = true
-    encrypt        = true
-  }
-}
--- terraform/hubs/staging/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "--envs-4852"
-    key            = "--hub/staging/terraform.tfstate"
-    region         = ""
-    use_lockfile   = true
-    encrypt        = true
-  }
-}
