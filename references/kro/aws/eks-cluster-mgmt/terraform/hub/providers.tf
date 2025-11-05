045411fdd2a3c9bfcfb4b6d74e9dac3de8cb2a88
Babasanmi Adeyemi <boadeyem@iu.edu>
2025-11-05 04:44:43 +0000

    testing

--- references/kro/aws/eks-cluster-mgmt/terraform/hub/providers.tf
+++ references/kro/aws/eks-cluster-mgmt/terraform/hub/providers.tf
@@ -1,10 +1,10 @@
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = local.cluster_info.cluster_endpoint
     cluster_ca_certificate = base64decode(local.cluster_info.cluster_certificate_authority_data)
 
-    exec {
+    exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
       # This requires the awscli to be installed locally where Terraform is executed
